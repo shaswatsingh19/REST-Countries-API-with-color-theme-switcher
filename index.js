694ef098169b869b99605bb141a211f106c4e011
shaswat singh <shaswatsingh.ose@gmail.com>
2022-09-29 20:52:49 +0530

    added search input logic

--- index.js
+++ index.js
@@ -1,4 +1,77 @@
 const countriesContainer = document.querySelector('.countries')
+const countryInput = document.getElementById('country-input')
+
+
+
+// All countries as page Loads
+function initalPageLoad(){
+    showData('https://restcountries.com/v3.1/all')
+}
+initalPageLoad()
+
+
+
+async function showData(url){
+    try{
+
+        const res  = await fetch(url)
+        const countryData = await res.json()
+
+        console.log(countryData)
+
+        const top5 = countryData.slice(0,5)
+        console.log(top5)
+    
+        countriesContainer.innerHTML = ''
+    
+        const temp=[]
+        top5.forEach(data => {
+            const div = document.createElement('div')
+            div.classList.add('country')
+    
+            div.innerHTML =  ` 
+                <img src=${data.flags.svg} alt="${data.name.common} flag image">
+                <div class="card-details">
+                    <h2>${data.name.common}</h2>
+                    <p>Population: <span>${data.population.toLocaleString('en-US')}</span> </p>
+                    <p>Region: <span>${data.region}</span> </p>
+                    <p>Capital: <span>${data.capital}</span> </p>
+                </div>
+            `
+            temp.push(div)
+        })
+        countriesContainer.append(...temp)
+
+    }catch(err){
+        
+        alert('No Data Found')
+
+    }
+
+
+}
+
+
+// Search Input  Event 
+countryInput.addEventListener('input', (e) =>{
+    const inputText = e.target.value
+    
+    const urlFromSearch = `https://restcountries.com/v3.1/name/${inputText}`
+
+    console.log(urlFromSearch)
+
+    if(inputText.length>= 3){
+        showData(urlFromSearch)
+    }else if(inputText.length == 0){
+        // If the search box is again empty then so all data
+        initalPageLoad()
+    }
+})
+
+
+
+
+
 // Theme 
 
 document.getElementById('theme').addEventListener('click',function(e){
@@ -9,53 +82,3 @@ document.getElementById('theme').addEventListener('click',function(e){
         e.target.textContent = 'Light Mode'
     }
 })
-
-
-async function onPageLoad(url){
-    const res  = await fetch(url)
-    const countryData = await res.json()
-
-    const top5 = countryData.slice(0,5)
-    console.log(top5)
-
-    const temp=[]
-    top5.forEach(data => {
-        const div = document.createElement('div')
-        div.classList.add('country')
-
-        div.innerHTML =  ` 
-            <img src=${data.flags.svg} alt="${data.name.common} flag image">
-            <div class="card-details">
-                <h2>${data.name.common}</h2>
-                <p>Population: <span>${data.population}</span> </p>
-                <p>Region: <span>${data.region}</span> </p>
-                <p>Capital: <span>${data.capital}</span> </p>
-            </div>
-        `
-        temp.push(div)
-    })
-    countriesContainer.append(...temp)
-
-}
-
-
-
-{/* 
-<div class="country">
-    <img src="https://flagcdn.com/ve.svg" alt="flag">
-    <div class="card-details">
-        <h2>Venezuela</h2>
-        <p>Population: <span>2314</span> </p>
-        <p>Region: <span>2314</span> </p>
-        <p>Capital: <span>2314</span> </p>
-    </div>
-</div> 
-*/}
-
-
-
-
-
-
-// All countries as page Loads
-onPageLoad('https://restcountries.com/v3.1/all')
